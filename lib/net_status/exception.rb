5f849b682cec89c5c3a53e97c13137cad49eebed
Алексей Игрычев <alexey.igrychev@flant.ru>
2016-02-26 11:56:15 +0300

    +

--- lib/net_status/exception.rb
+++ lib/net_status/exception.rb
@@ -1,15 +1,17 @@
 class NetStatus::Exception < Exception
-  attr_reader :net_status
-
-  def initialize(message_or_net_status=nil)
-    @net_status = (message_or_net_status.nil? or message_or_net_status.is_a? String) ?
-        { error: :error, code: :exception, message: message_or_net_status, data: { backtrace: self.backtrace } } :
-        message_or_net_status
-    @net_status[:data] ||= {}
-    @net_status[:data][:backtrace] ||= self.backtrace
+  def initialize(net_status={})
+    @net_status = net_status
+    @net_status[:error] ||= :error
     super()
   end
-  
+
+  def net_status
+    @net_status.dup.tap do |res|
+      res[:data] = (res[:data] || {}).dup
+      res[:data][:backtrace] ||= self.backtrace
+    end
+  end
+
   def message
     net_status_short.to_s
   end
--- lib/net_status/version.rb
+++ lib/net_status/version.rb
@@ -1,3 +1,3 @@
 module NetStatus
-  VERSION = '0.2.3'
+  VERSION = '0.2.4'
 end
