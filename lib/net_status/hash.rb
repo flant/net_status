50afd423a167da16c9562fe50821c89c17a0f30f
Алексей Игрычев <alexey.igrychev@flant.ru>
2016-03-03 20:09:29 +0300

    fixed

--- lib/net_status/hash.rb
+++ lib/net_status/hash.rb
@@ -5,7 +5,7 @@ class NetStatus::Hash
     end
 
     def net_status_ok!
-      raise NetStatus::Exception, self unless net_status_ok?
+      net_status_ok? ? self : raise NetStatus::Exception, self
     end
   end
 end
