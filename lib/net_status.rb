5a91caf2daaa74985699ede3bc5a9f8ac60a71d8
Alexey Igrychev <pes.sutuliy@gmail.com>
2016-03-11 14:28:00 +0300

    fixed

--- lib/net_status.rb
+++ lib/net_status.rb
@@ -7,6 +7,7 @@ module NetStatus
   def self.normalize(net_status, context: nil)
     code = net_status[:code]
     code ||= net_status[:error] if net_status.include? :error
+    net_status[:data] ||= {}
     net_status[:message] = I18n.t [:net_status, context, code].join('.'), [:net_status, code].join('.'), **net_status[:data], raise: true rescue nil
     net_status
   end
